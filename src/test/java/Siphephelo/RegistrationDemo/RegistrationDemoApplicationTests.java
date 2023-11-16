bd1be4c612feee5bcd1c3bac5a374cb3306a0e36
Siphephelo Myeni <sipheanele@gmail.com>
2023-11-16 21:14:24 +0200

    Initial commit

---
+++ src/test/java/Siphephelo/RegistrationDemo/RegistrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Siphephelo.RegistrationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
